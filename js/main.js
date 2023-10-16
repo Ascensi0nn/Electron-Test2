63e1172ba1565ff537103560dc1640ccd03d08c6
Owen Luts <oluts47@gmail.com>
2023-10-16 12:54:06 -0400

    Basic Electron Setup w/ preloading

--- js/main.js
+++ js/main.js
@@ -7,20 +7,24 @@ const createWindow = () => {
 		width: 800,
 		height: 600,
 		webPreferences: {
-		preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, 'preload.js')
 		}
 	})
 
 	mainWindow.menuBarVisible = false;
-	mainWindow.loadFile('html/index.html');
+	mainWindow.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
 	createWindow()
 
 	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
 	})
+
+	
 })
 app.on('window-all-closed', () => {
   	if (process.platform !== 'darwin') {
